caf4effcc4f96f71f43b43b307f47c9ea65ab7f4
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-22 15:35:53 -0700

    got data to hold in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import UserForm from "./Components/Form";
-import data from './data';
+
 
 function App() {
   return (
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import axios from "axios";
-import { Form, Field, withFormik, Formik, yupToFormErrors} from "formik";
+import { Form, Field, withFormik} from "formik";
 import * as Yup from "yup";
 
 const UserForm = ({errors, touched, values}) =>{
     return(
         <div className="form-parent">
-            <Formik>
             <Form>
+                <label>
+                Name
                 <Field name="user" type="text" placeholder="User Name"/>
                 {touched.user && errors.user && (
-                    <p className="error">{errors.user}</p>
+                    <p>{errors.user}</p>
                 )}
-                <Field name="email" type="email" placeholder="email@email.com"/>
-                {touched.email && errors.email && (
-                    <p className="error">{errors.email}</p>
+                </label>
+                <label>
+                    Email
+                    <Field name="email" type="email" placeholder="email@email.com" required/>
+                </label>  
+                <label>
+                    Password
+                    <Field name="password" type="password" placeholder="password"/>
+                    {touched.password && errors.password && (
+                    <p>{errors.password}</p>
                 )}
-                <Field name="password" type="password" placeholder="password"/>
-                {touched.password && errors.password && (
-                    <p className="error">{errors.password}</p>
-                )}
+                </label>
+                
                 <label className="checkmark-container">
                     Terms of Service
                     <Field
                         type="checkbox"
                         name="terms"
-                        // checked={values.terms}
+                        checked={values.terms}
                     />
                     <span className="checkmark"/>
                 </label>
                 <button>Submit!</button>
             </Form>
-            </Formik>
         </div>
     );
 }
@@ -45,14 +50,13 @@ const FormikUserForm = withFormik({
         };
     },
     validateSchema: Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string().required(),
-        password: Yup.string().required(),
+        user: Yup.string().required("required"),
+        password: Yup.string().required("required")
     }),
 
     handleSubmit(values, { setStatus }) {
         axios
-          .post("<https://reqres.in/api/users/>", values)
+          .post("https://reqres.in/api/users/", values)
           .then(res => {
             setStatus(res.data);
             console.log(values);
